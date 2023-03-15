ca743d08a9ea265cba619776595c6ac8be298d9b
RaphaelIT7 <raphael.i.t@outlook.de>
2023-03-16 00:37:48 +0100

    [GameEventManager] Release 0.2
    [+] The Event data get now pushed to Lua.

--- gmod-gameeventmanager/projects/windows/vs2022/Base.cpp
+++ gmod-gameeventmanager/projects/windows/vs2022/Base.cpp
@@ -17,22 +17,196 @@ public:
 	{
 		const char* EventName = event->GetName();
 
-		Msg(event->GetName());
-		Msg("\n");
-		Msg(event->GetString("message"));
-		Msg("\n");
 		GlobalLUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
 		GlobalLUA->GetField(-1, "hook");
 		GlobalLUA->GetField(-1, "Run");
-			GlobalLUA->PushString(event->GetName());
+		GlobalLUA->PushString(event->GetName());
+		GlobalLUA->CreateTable();
+		if (strcmp(EventName, "OnRequestFullUpdate") == 0) {
+				GlobalLUA->PushString(event->GetString("networkid"));
+				GlobalLUA->SetField(-2, "networkid");
+				GlobalLUA->PushString(event->GetString("name"));
+				GlobalLUA->SetField(-2, "name");
+				GlobalLUA->PushNumber(event->GetInt("userid"));
+				GlobalLUA->SetField(-2, "userid");
+				GlobalLUA->PushNumber(event->GetInt("index"));
+				GlobalLUA->SetField(-2, "index");
+		}
+		else if (strcmp(EventName, "achievement_earned") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("achievement"));
+			GlobalLUA->SetField(-2, "achievement");
+			GlobalLUA->PushNumber(event->GetInt("player"));
+			GlobalLUA->SetField(-2, "player");
+		}
+		else if (strcmp(EventName, "achievement_event") == 0) {
+			GlobalLUA->PushString(event->GetString("achievement_name"));
+			GlobalLUA->SetField(-2, "achievement_name");
+			GlobalLUA->PushNumber(event->GetInt("cur_val"));
+			GlobalLUA->SetField(-2, "cur_val");
+			GlobalLUA->PushNumber(event->GetInt("max_val"));
+			GlobalLUA->SetField(-2, "max_val");
+		}
+		else if (strcmp(EventName, "break_breakable") == 0) {
+			GlobalLUA->PushString(event->GetString("entindex"));
+			GlobalLUA->SetField(-2, "entindex");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushNumber(event->GetInt("material"));
+			GlobalLUA->SetField(-2, "material");
+		}
+		else if (strcmp(EventName, "break_prop") == 0) {
+			GlobalLUA->PushString(event->GetString("entindex"));
+			GlobalLUA->SetField(-2, "entindex");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+		}
+		else if (strcmp(EventName, "client_disconnect") == 0) {
+			GlobalLUA->PushString(event->GetString("message"));
+			GlobalLUA->SetField(-2, "message");
+		}
+		else if (strcmp(EventName, "entity_killed") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("entindex_inflictor"));
+			GlobalLUA->SetField(-2, "entindex_inflictor");
+			GlobalLUA->PushNumber(event->GetInt("entindex_attacker"));
+			GlobalLUA->SetField(-2, "entindex_attacker");
+			GlobalLUA->PushNumber(event->GetInt("damagebits"));
+			GlobalLUA->SetField(-2, "damagebits");
+			GlobalLUA->PushNumber(event->GetInt("entindex_killed"));
+			GlobalLUA->SetField(-2, "entindex_killed");
+		}
+		else if (strcmp(EventName, "flare_ignite_npc") == 0) {
+			GlobalLUA->PushString(event->GetString("entindex"));
+			GlobalLUA->SetField(-2, "entindex");
+		}
+		else if (strcmp(EventName, "host_quit") == 0) {
+		}
+		else if (strcmp(EventName, "player_activate") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+		}
+		else if (strcmp(EventName, "player_changename") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushString(event->GetString("oldname"));
+			GlobalLUA->SetField(-2, "oldname");
+			GlobalLUA->PushString(event->GetString("newname"));
+			GlobalLUA->SetField(-2, "newname");
+		}
+		else if (strcmp(EventName, "player_connect") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("bot"));
+			GlobalLUA->SetField(-2, "bot");
+			GlobalLUA->PushString(event->GetString("networkid"));
+			GlobalLUA->SetField(-2, "networkid");
+			GlobalLUA->PushString(event->GetString("name"));
+			GlobalLUA->SetField(-2, "name");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushNumber(event->GetInt("index"));
+			GlobalLUA->SetField(-2, "index");
+			GlobalLUA->PushString(event->GetString("address"));
+			GlobalLUA->SetField(-2, "address");
+		}
+		else if (strcmp(EventName, "player_connect_client") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("bot"));
+			GlobalLUA->SetField(-2, "bot");
+			GlobalLUA->PushString(event->GetString("networkid"));
+			GlobalLUA->SetField(-2, "networkid");
+			GlobalLUA->PushString(event->GetString("name"));
+			GlobalLUA->SetField(-2, "name");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushNumber(event->GetInt("index"));
+			GlobalLUA->SetField(-2, "index");
+		}
+		else if (strcmp(EventName, "player_disconnect") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("bot"));
+			GlobalLUA->SetField(-2, "bot");
+			GlobalLUA->PushString(event->GetString("networkid"));
+			GlobalLUA->SetField(-2, "networkid");
+			GlobalLUA->PushString(event->GetString("name"));
+			GlobalLUA->SetField(-2, "name");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushString(event->GetString("reason"));
+			GlobalLUA->SetField(-2, "reason");
+		}
+		else if (strcmp(EventName, "player_hurt") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("health"));
+			GlobalLUA->SetField(-2, "health");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushNumber(event->GetInt("attacker"));
+			GlobalLUA->SetField(-2, "attacker");
+		}
+		else if (strcmp(EventName, "player_info") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("friendsid"));
+			GlobalLUA->SetField(-2, "friendsid");
+			GlobalLUA->PushNumber(event->GetInt("index"));
+			GlobalLUA->SetField(-2, "index");
+			GlobalLUA->PushNumber(event->GetInt("bot"));
+			GlobalLUA->SetField(-2, "bot");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushString(event->GetString("name"));
+			GlobalLUA->SetField(-2, "name");
+			GlobalLUA->PushString(event->GetString("networkid"));
+			GlobalLUA->SetField(-2, "networkid");
+		}
+		else if (strcmp(EventName, "player_say") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushString(event->GetString("text"));
+			GlobalLUA->SetField(-2, "text");
+		}
+		else if (strcmp(EventName, "player_spawn") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+		}
+		else if (strcmp(EventName, "ragdoll_dissolved") == 0) {
+			GlobalLUA->PushNumber(event->GetInt("entindex"));
+			GlobalLUA->SetField(-2, "entindex");
+		}
+		else if (strcmp(EventName, "server_addban") == 0) {
+			GlobalLUA->PushString(event->GetString("networkid"));
+			GlobalLUA->SetField(-2, "networkid");
+			GlobalLUA->PushString(event->GetString("name"));
+			GlobalLUA->SetField(-2, "name");
+			GlobalLUA->PushNumber(event->GetInt("userid"));
+			GlobalLUA->SetField(-2, "userid");
+			GlobalLUA->PushString(event->GetString("ip"));
+			GlobalLUA->SetField(-2, "ip");
+			GlobalLUA->PushString(event->GetString("duration"));
+			GlobalLUA->SetField(-2, "duration");
+			GlobalLUA->PushString(event->GetString("by"));
+			GlobalLUA->SetField(-2, "by");
+			GlobalLUA->PushBool(event->GetBool("kicked"));
+			GlobalLUA->SetField(-2, "kicked");
+		}
+		else if (strcmp(EventName, "server_cvar") == 0) {
+			GlobalLUA->PushString(event->GetString("cvarname"));
+			GlobalLUA->SetField(-2, "cvarname");
+			GlobalLUA->PushString(event->GetString("cvarvalue"));
+			GlobalLUA->SetField(-2, "cvarvalue");
+		}
+		else if (strcmp(EventName, "server_removeban") == 0) {
+			GlobalLUA->PushString(event->GetString("networkid"));
+			GlobalLUA->SetField(-2, "networkid");
+			GlobalLUA->PushString(event->GetString("ip"));
+			GlobalLUA->SetField(-2, "ip");
+			GlobalLUA->PushString(event->GetString("by"));
+			GlobalLUA->SetField(-2, "by");
+		}
 
-		GlobalLUA->Call(1, 0);
+		GlobalLUA->Call(2, 0);
 		GlobalLUA->Pop(2);
 	}
 };
 
 LUA_FUNCTION_STATIC(Listen) {
-	gameeventmanager->AddListener(new CustomGameEventListener, LUA->CheckString(0), false);
+	const char* name = LUA->CheckString(1);
+	//Msg(name); 
+	//Msg("\n");
+	gameeventmanager->AddListener(new CustomGameEventListener, name, false);
 
 	return 1;
 }
---
+++ gmod-gameeventmanager/projects/windows/vs2022/x86/Release/copy.bat
@@ -0,0 +1,3 @@
+@echo off
+
+XCopy "gmcl_gameevent_win32.dll" "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod\lua\bin\gmsv_gameevent_win32.dll" /c/Y
