fe6bcee57eb89b9f6a7e2b01cdb7d0b3c6f56c26
Raphael <64648134+RaphaelIT7@users.noreply.github.com>
2023-06-01 11:33:19 +0200

    Update util.h

--- source/util.h
+++ source/util.h
@@ -8,11 +8,11 @@ using namespace GarrysMod::Lua;
 extern GarrysMod::Lua::ILuaBase* GlobalLUA;
 extern void Start_Table();
 
-extern void Add_Func(CFunc, char*);
+extern void Add_Func(CFunc, const char*);
 extern void Add_Func(CFunc, std::string);
 
-extern void Finish_Table(char*);
+extern void Finish_Table(const char*);
 extern void Finish_Table(std::string);
 
-extern void LuaPrint(char*);
+extern void LuaPrint(const char*);
 extern void LuaPrint(std::string);
